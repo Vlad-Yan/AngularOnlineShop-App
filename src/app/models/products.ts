cd17cecb8e9d936a1c4a9a4428e01a8f385f5181
Vlad-Yan <vladik_yanchevskiy@mail.ru>
2023-02-24 11:31:05 +0500

    Добавил Json-server

---
+++ src/app/models/products.ts
@@ -0,0 +1,15 @@
+export interface IProducts {
+  id: number,
+  title: string,
+  price: number,
+  image?: string,
+  characterizations: IProductsCharacterizations;
+}
+
+export interface IProductsCharacterizations {
+  brand: string,
+  gender: string,
+  height: string,
+  style: string,
+  material: string;
+}
