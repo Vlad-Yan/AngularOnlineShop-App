6855217ce6ebd1008582a29fb327151d0cb6b721
Vlad-Yan <vladik_yanchevskiy@mail.ru>
2023-02-26 14:01:08 +0500

    Добавил Resolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductResolver } from './services/product.resolver';
 import { BasketComponent } from './components/basket/basket.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductsComponent } from './components/products/products.component';
@@ -8,7 +9,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: '', component: BaseComponent},
   {path: 'products', component: ProductsComponent},
-  {path: 'product/:id', component: ProductDetailsComponent},
+  {path: 'product/:id', component: ProductDetailsComponent, resolve: { data: ProductResolver }},
   {path: 'basket', component: BasketComponent},
 
   {path: '**', redirectTo: "", component: BaseComponent}
